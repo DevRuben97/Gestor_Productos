c2ccfb79c64cc6c077cfaa567566cf2299b8b6aa
Ruben A. Batista Santos <alexbatista158@hotmail.com>
2020-01-06 21:31:15 -0400

    Diseño del view Login

--- src/helpers/formValidations.js
+++ src/helpers/formValidations.js
@@ -8,3 +8,8 @@ export const PRODUCT_SCHEMA= yup.object().shape({
     Category: yup.string().required('La categoria es requerido'),
     Provider: yup.string().required('El Proveedor es requerido')
 })
+
+export const USER_LOGIN_SCHEMA= yup.object().shape({
+    user: yup.string().email('Ingresa un correo electronico valido'),
+    password: yup.string().required('La contraseña es requerida')
+})
--- src/views/Login.js
+++ src/views/Login.js
@@ -1,13 +1,69 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Formik } from "formik";
+import {USER_LOGIN_SCHEMA} from '../helpers/formValidations';
 
-const Login= (props)=>{
+const Login = props => {
+  const [UserLogin, setLogin] = useState({
+    user: "",
+    password: ""
+  });
 
-    return (
-        <div className="container">
-            <h2 className="text-center">Iniciar Sessión</h2>
+  function Login(value) {
+      localStorage.setItem('logged','true');
+      
+  }
+
+  return (
+    <div className="Login">
+      <div className="login-container">
+        <div className="card card-body-border">
+          <div className="card-body">
+            <h2 className="text-center">Bienvenido</h2>
+            <label>Ingresa tus credenciales de acceso</label>
+            <div className="row">
+              <div className="col">
+                <Formik
+                  initialValues={UserLogin}
+                  onSubmit={values => Login(values)}
+                  validationSchema={USER_LOGIN_SCHEMA}
+                >
+                  {({ handleSubmit, handleChange, values, errors }) => (
+                    <form onSubmit={handleSubmit}>
+                      <label>Correo</label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="tucorreo@ejemplo.com"
+                        onChange={handleChange('user')}
+                      />
+                      <label className="ValidatetionError">{errors.user}</label>
+                      <br />
+                      <label>Contraseña</label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Ingrese su contraseña"
+                        onChange={handleChange('password')}
+                      />
+                      <label className="ValidatetionError">{errors.password}</label>
+                      <br />
+                      <button 
+                      className="btn btn-primary btn-lg btn-block" 
+                      type="submit"
+                      disabled={values.user=== ''|| values.password===''}
+                      >
+                        <i class="fas fa-arrow-circle-right"></i> Iniciar Sessión
+                      </button>
+                    </form>
+                  )}
+                </Formik>
+              </div>
+            </div>
+          </div>
         </div>
-    )
-
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
