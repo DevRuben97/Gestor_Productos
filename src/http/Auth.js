a45a0be0fa475245b5c452598c99a5ae5562bce8
Ruben A. Batista Santos <alexbatista158@hotmail.com>
2020-01-27 01:30:02 -0400

    Diseño de la pantalla de perfil de usuario

--- src/http/Auth.js
+++ src/http/Auth.js
@@ -4,3 +4,7 @@ import http from './http';
 export async function login(values){
     return await http.post('/Auth/login',values);
 }
+
+export async function GetUserInfo(id){
+    return await http.get(`/Auth/UserProfile/${id}`);
+}
--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -1,15 +1,43 @@
-import React from 'react';
-
+import React, { useEffect, useContext, useState } from 'react';
+import {GetUserInfo} from '../http/Auth';
+import UserInfo from '../Context/UserContext';
+import Avatar from 'react-avatar';
 
 export default function UserProfile(props){
 
+    const {User} = useContext(UserInfo);
+    const [Profile, SetProfile]= useState({});
+
+    useEffect(()=>{
+        async function GetProfile(){
+            
+            const {data}= await GetUserInfo(User.Id);
+            SetProfile(data.Data);
+        }
+        GetProfile();
+    },[])
     return (
         <div className="container">
             <h2 className="text-center"><i class="fas fa-id-card"></i> Perfil del Usuario</h2>
 
            <div className="card">
                <div className="card-body">
-                   
+                   <div style={{justifyContent: 'center', display: 'flex'}}>
+                        <Avatar name= {Profile.FullName} round='10px' size= '80px'/>
+                   </div>
+                   <div>
+                        <h2 className="text-center">{Profile.FullName}</h2>
+                   </div>
+                   <br />
+                   <div style={{justifyContent: 'center', display: 'flex'}}>
+                        <div>
+                        <label><strong>Correo: </strong> {Profile.Email}</label>
+                        <br/>
+                        <label><strong>Identificación: </strong> {Profile.Identification}</label>
+                        <br/>
+                        <label><strong>Telefono: </strong> {Profile.Phone}</label>
+                        </div>
+                   </div>
                </div>
            </div>
         </div>
